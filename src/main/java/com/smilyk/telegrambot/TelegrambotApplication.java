e4b34d06fbc76006a7a726fb99b34e0769feeb86
smilyk <smilyk1982@gmail.com>
2021-04-06 12:30:48 +0300

    TB-0: Create skeleton SpringBoot

---
+++ src/main/java/com/smilyk/telegrambot/TelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.smilyk.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegrambotApplication.class, args);
+	}
+
+}
